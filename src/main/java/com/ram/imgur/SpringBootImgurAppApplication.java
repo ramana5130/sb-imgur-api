345b6eb0fbd141b241da7d396313ed9f8db6ed0d
ramanareddy <venkataramanareddygopu@gmail.com>
2023-05-27 11:19:55 +0530

    Basic Setup Done by adding below springboot starters 1. web 2. data-jpa 3. h2 database 4. lombok

---
+++ src/main/java/com/ram/imgur/SpringBootImgurAppApplication.java
@@ -0,0 +1,13 @@
+package com.ram.imgur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImgurAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImgurAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ram/imgur/SpringBootImgurAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.imgur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootImgurAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
